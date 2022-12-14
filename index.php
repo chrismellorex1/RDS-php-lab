0091c1842d982bb41d13a1953a0333615c48dc48
Christopher Mello <chrismellorex1@yahoo.com>
2022-12-14 15:00:36 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+Lets connect to our RDS server
+
+
+
+
+<?php
+
+
+include 'includes/connection2022.php';
+
+
+?>
+
+    
+</body>
+</html>
