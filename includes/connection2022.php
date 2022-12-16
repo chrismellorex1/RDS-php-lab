4daac67d5b70258a17fa368cdc95f4e385ef6d5f
Christopher Mello <chrismellorex1@yahoo.com>
2022-12-16 00:08:32 +0000

    connection2022.php update

--- includes/connection2022.php
+++ includes/connection2022.php
@@ -1,11 +1,46 @@
 <?php
 
 
-$con=mysqli_connect('database-2022.comd0yvbqjgv.us-east-1.rds.amazonaws.com', 'admin', 'yourpassword','metadata','3306');
+$Site = $_POST["Site"];
+$weather     = $_POST["weather"];
+$date    = filter_input(INPUT_POST, "date" , FILTER_VALIDATE_INT);
+$temperature    = filter_input(INPUT_POST, "temperature" , FILTER_VALIDATE_INT);
+$windspeed    = filter_input(INPUT_POST, "windspeed" , FILTER_VALIDATE_INT);
+
+
+$con=mysqli_connect('database-2022.comd0yvbqjgv.us-east-1.rds.amazonaws.com', 'admin', 'un!g*bun1984W','metadata','3306');
+
+
+
 
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "\n Failed to connect to MySQL: \n" . mysqli_connect_error();
 }
+
+echo "Connection is active";
+
+var_dump($Site , $date, $temperature, $windspeed, $weather);
+
+
+
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+echo "__________________________________________________________________";
+
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+echo "                                                          ";
+
+
+
 
 $sql = "SELECT * FROM `weatherobs` ";
 
